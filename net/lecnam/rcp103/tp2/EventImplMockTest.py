0a7c3277eb3b4c57ab2a08402fde3238f34c1ccc
ezYakaEagle442 <stepin.devops@outlook.com>
2026-05-06 15:18:37 +0200

    MagicMock(spec=EventImpl) test success

--- net/lecnam/rcp103/tp2/EventImplMockTest.py
+++ net/lecnam/rcp103/tp2/EventImplMockTest.py
@@ -7,26 +7,26 @@ import traceback
 import logging
 
 from unittest.mock import MagicMock
-from net.lecnam.rcp103.tp2.EventImpl import EventImplMockTest
+from net.lecnam.rcp103.tp2.EventImpl import EventImpl
 
 from net.lecnam.rcp103.tp2.EventImpl import EventImpl
 from net.lecnam.rcp103.tp2.IEvent import IEvent
-from .IMessage import IMessage
+from net.lecnam.rcp103.tp2.IMessage import IMessage
 from net.lecnam.rcp103.tp2.MessageImpl import MessageImpl
 from net.lecnam.rcp103.tp2.EventType import EventType
 
-# Create a mock instance of SimulateurImpl
+# Create a mock instance of EventImpl
 mock_sim = MagicMock(spec=EventImpl)
 
 # Example: set return value for calcul
 mock_sim.get_event_time.return_value = 1.42
 
-msg = MessageImpl(1, "Test message", "Alice", "Bob")
+msg = MessageImpl(1, "Alice", "Bob", 1.64)
 
 # Use the mock in your test
 result = mock_sim.get_event_time()
 
-print("Mocked calcul result:", result)  # Output: Mocked calcul result: 1,42
+print("Mocked result:", result)  # Output: Mocked calcul result: 1,42
 
 # You can also assert calls
 mock_sim.get_event_time.assert_called_with()
