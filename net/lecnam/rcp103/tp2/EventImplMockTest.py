c44c8f8ab78b0cc7853945ea40cf550ee9f256af
ezYakaEagle442 <stepin.devops@outlook.com>
2026-05-07 09:24:29 +0200

    wrap code with 'class EventImplMockTest(unittest.TestCase)' and add assertions

--- net/lecnam/rcp103/tp2/EventImplMockTest.py
+++ net/lecnam/rcp103/tp2/EventImplMockTest.py
@@ -6,33 +6,42 @@
 import traceback
 import logging
 
+import unittest
 from unittest.mock import MagicMock
-from net.lecnam.rcp103.tp2.EventImpl import EventImpl
-
 from net.lecnam.rcp103.tp2.EventImpl import EventImpl
 from net.lecnam.rcp103.tp2.IEvent import IEvent
 from net.lecnam.rcp103.tp2.IMessage import IMessage
 from net.lecnam.rcp103.tp2.MessageImpl import MessageImpl
 from net.lecnam.rcp103.tp2.EventType import EventType
 
-# Create a mock instance of EventImpl
-mock_sim = MagicMock(spec=EventImpl)
+class EventImplMockTest(unittest.TestCase):
 
-# Example: set return value for calcul
-mock_sim.get_event_time.return_value = 1.42
-mock_sim.get_event_type.return_value = "SEND_MSG" # "Fake Foo Event"
-mock_sim.get_event_id.return_value = 42
+    def test_create_class_call_method(self):
+        # Create a mock instance of EventImpl
+        mock_sim = MagicMock(spec=EventImpl)
 
-msg = MessageImpl(1, "Alice", "Bob", 1.64)
+        # Example: set return value for calcul
+        msg = MessageImpl(1, "Alice", "Bob", 1.64)
+        mock_sim.get_message.return_value = msg
 
-# Use the mock in your test
-t = mock_sim.get_event_time()
-type = mock_sim.get_event_type()
-id = mock_sim.get_event_id()
+        mock_sim.get_event_time.return_value = 1.42
+        mock_sim.get_event_type.return_value = "SEND_MSG" # "Fake Foo Event"
+        mock_sim.get_event_id.return_value = 42
 
-print("Mocked id:", id)  # Output: Mocked calcul result: 1,42
-print("Mocked type:", type)
-print("Mocked t:", t)
+        # Use the mock in your test
+        t = mock_sim.get_event_time()
+        type = mock_sim.get_event_type()
+        id = mock_sim.get_event_id()
 
-# You can also assert calls
-mock_sim.get_event_time.assert_called_with()
+        print("Mocked id:", id)
+        print("Mocked type:", type)
+        print("Mocked t:", t)
+
+        # You can also assert calls
+        mock_sim.get_event_time.assert_called_once()
+        self.assertEqual(t, 1.42)
+        self.assertEqual(type, "SEND_MSG")
+        self.assertEqual(id, 42)
+
+if __name__ == '__main__':
+    unittest.main()
